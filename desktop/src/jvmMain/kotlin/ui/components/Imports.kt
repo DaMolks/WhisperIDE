b5c4ab1a80abcde7e72e5e2a71e84f4f0bec7e8a
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-01 20:43:20 +0100

    Ajout du fichier d'imports communs

---
+++ desktop/src/jvmMain/kotlin/ui/components/Imports.kt
@@ -0,0 +1,34 @@
+package ui.components
+
+// Desktop
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+// Foundation
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+
+// Material
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+// Runtime
+import androidx.compose.runtime.*
+
+// UI
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// Project
+import com.whisperide.shared.project.*
+import github.*
