339cd260e29820f14bd50569733ae2f51a93e444
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-01 15:18:39 +0100

    Ajout des repositories Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,11 @@ plugins {
 
 group = "com.whisperIDE"
 version = "1.0-SNAPSHOT"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
