43736e5962742e63d17b5971fa0522fc7913dac7
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-01 19:57:00 +0100

    Préparation pour le Main

--- desktop/src/jvmMain/kotlin/MainKt.kt
+++ desktop/src/jvmMain/kotlin/MainKt.kt
@@ -1,9 +1,18 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.WindowPosition
-import androidx.compose.ui.window.rememberWindowState
-import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ui.MainView
 
 fun main() = application {
