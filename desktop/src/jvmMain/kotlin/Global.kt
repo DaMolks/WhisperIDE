49a85e3b44777dd3e8e039f2242af8f99816fc5e
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 01:48:13 +0100

    Clean up Global.kt

--- desktop/src/jvmMain/kotlin/Global.kt
+++ desktop/src/jvmMain/kotlin/Global.kt
@@ -1,13 +1,1 @@
-import com.whisperide.shared.project.ProjectManager
-import github.GithubAuth
-import github.GithubManager
-import java.io.File
-
-// Instances globales pour la gestion de GitHub
-val githubAuth = GithubAuth()
-val githubManager = GithubManager(githubAuth)
-
-// Instance globale pour la gestion des projets
-val projectManager = ProjectManager(
-    workspacePath = File(System.getProperty("user.home"), "WhisperIDE/Projects")
-)
+// Fichier vide - sera supprimé
