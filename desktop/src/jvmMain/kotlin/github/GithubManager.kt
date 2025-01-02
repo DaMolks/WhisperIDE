8bd155e9676a1298a91535fd25e24e4f0d7af66f
DaMolks <108116237+DaMolks@users.noreply.github.com>
2025-01-02 01:58:00 +0100

    Add GitHub token validation

--- desktop/src/jvmMain/kotlin/github/GithubManager.kt
+++ desktop/src/jvmMain/kotlin/github/GithubManager.kt
@@ -1,15 +1,35 @@
 package github
 
-import java.io.File
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 import java.time.LocalDate
+import java.io.File
 
 class GithubManager(private val auth: GithubAuth = githubAuth) {
-    fun initializeGithub(token: String, expirationDate: LocalDate): Boolean {
+    private val client = HttpClient(CIO)
+
+    suspend fun initializeGithub(token: String, expirationDate: LocalDate): Boolean {
         return try {
-            // TODO: Vérifier la validité du token avec l'API GitHub
-            auth.setCredentials(token, expirationDate)
-            true
+            // Vérifie le token avec l'API GitHub
+            val response = client.get("https://api.github.com/user") {
+                headers {
+                    append(HttpHeaders.Authorization, "token $token")
+                    append(HttpHeaders.Accept, "application/vnd.github.v3+json")
+                    append(HttpHeaders.UserAgent, "WhisperIDE")
+                }
+            }
+
+            if (response.status == HttpStatusCode.OK) {
+                auth.setCredentials(token, expirationDate)
+                true
+            } else {
+                false
+            }
         } catch (e: Exception) {
+            e.printStackTrace()
             false
         }
     }
